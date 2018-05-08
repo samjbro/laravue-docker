a246423b3a837133a339fbc2de07f5e557ffb2b3
Sam Broughton <samb@DEVE-iMac.local>
2018-05-08 17:12:08 +0100

    initial setup complete

---
+++ develop.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+export APP_PORT=${APP_PORT:-80}
+
+COMPOSE_FILE="dev"
+
+COMPOSE="docker-compose -f docker-compose.${COMPOSE_FILE}.yml"
+
+if [ $# -gt 0 ]; then
+    if [ "$1" == "composer" ]; then
+        shift 1
+        $COMPOSE run --rm \
+            -w /var/www/html \
+            app \
+            composer "$@"
+    elif [ "$1" == "yarn" ]; then
+        shift 1
+        $COMPOSE run --rm \
+            -w /var/www/html \
+            node \
+            yarn "$@"
+    elif [ "$1" == "npm" ]; then
+        shift 1
+        $COMPOSE run --rm \
+            -w /var/www/html \
+            node \
+            npm "$@"
+    else
+        $COMPOSE "$@"
+    fi
+else
+    $COMPOSE ps
+fi
